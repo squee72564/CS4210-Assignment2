6b000bb1239edaa8d81460da9c6ac12e47d5433d
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-02-24 14:59:44 -0800

    Completed 1NN calculations and model average for 3.e

--- knn.py
+++ knn.py
@@ -1,9 +1,10 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Alexander Rodriguez
+# FILENAME: knn.py
+# SPECIFICATION: Program reads binary_points.csv and outputs the LOO-CV error rate
+#    for 1NN
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 20 min
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
@@ -21,36 +22,49 @@ with open('binary_points.csv', 'r') as csvfile:
       if i > 0: #skipping the header
          db.append (row)
 
-#loop your data to allow each instance to be your test set
-for ? in db:
 
+#loop your data to allow each instance to be your test set
+correctPred = 0
+
+for i, point in enumerate(db):
     #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
     # float to avoid warning messages
-    #--> add your Python code here
-    # X =
+    X = []
+    for j, point in enumerate(db):
+        if j != i:
+            X.append([float(point[0]), float(point[1])])
 
     #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. For instance, Y = [1, 2, ,...]. Convert each
     #  feature value to float to avoid warning messages
-    #--> add your Python code here
-    # Y =
+    Y = []
+    for k, point in enumerate(db):
+        if k != i:
+            if point[-1] == '-':
+                Y.append(0)
+            else:
+                Y.append(1)
 
     #store the test sample of this iteration in the vector testSample
-    #--> add your Python code here
-    #testSample =
+    testSample = [float(str) for str in db[i][:-1]]
+    testClass = None
+    if db[i][-1] == '-':
+        testClass = 0
+    else:
+        testClass = 1
 
     #fitting the knn to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
     clf = clf.fit(X, Y)
 
     #use your test sample in this iteration to make the class prediction. For instance:
-    #class_predicted = clf.predict([[1, 2]])[0]
-    #--> add your Python code here
+    class_predicted = clf.predict([testSample])[0]
 
     #compare the prediction with the true label of the test instance to start calculating the error rate.
-    #--> add your Python code here
-
+    if class_predicted == testClass:
+        correctPred += 1
+        
 #print the error rate
-#--> add your Python code here
+print(f'Accuracy = {correctPred / len(db)}')
 
 
 
